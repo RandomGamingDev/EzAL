c481816c23c10bc86882dd6f11b08b33a3173604
RandomGamingDev <ethanshiyi1123@gmail.com>
2022-06-25 09:57:13 -0400

    updated SoundDevice.cpp's destructor

--- EzAL/SoundDevice.cpp
+++ EzAL/SoundDevice.cpp
@@ -7,11 +7,13 @@
 			std::cout << "ERROR: Failed to get the device!\n";
 			return;
 		}
+
 		context = alcCreateContext(device, nullptr);
 		if (!context) {
 			std::cout << "ERROR: Failed to create the contesxt!\n";
 			return;
 		}
+
 		if (!alcMakeContextCurrent(context)) {
 			std::cout << "ERROR: Failed to make the context current!\n";
 			return;
@@ -19,8 +21,20 @@
 	}
 
 	SoundDevice::~SoundDevice() {
-		alcMakeContextCurrent(nullptr);
+		if (!alcMakeContextCurrent(nullptr)) {
+			std::cout << "Failed to set the current context to nullptr";
+			return;
+		}
+
 		alcDestroyContext(context);
-		alcCloseDevice(device);
+		if (context) {
+			std::cout << "Failed to destroy the context";
+			return;
+		}
+
+		if (!alcCloseDevice(device)) {
+			std::cout << "Failed to close the sound device";
+			return;
+		}
 	}
 #endif
