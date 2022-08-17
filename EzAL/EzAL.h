4814940eefeef174b3e8583cc28263a82865c8a6
RandomGamingDev <ethanshiyi1123@gmail.com>
2022-08-17 12:18:17 -0400

    library should now be submodule

--- EzAL/EzAL.h
+++
@@ -1,7 +0,0 @@
-#ifndef EZAL_HEADER
-#define EZAL_HEADER
-
-#include "ALlibs.h"
-#include "EzALLibs.h"
-
-#endif
--- EzAL/EzALlibs.h
+++
@@ -1,8 +0,0 @@
-#ifndef EZAL_LIBS_HEADER
-#define EZAL_LIBS_HEADER
-
-#include "SoundDevice.h"
-#include "SoundSource.h"
-#include "SoundBuffer.h"
-
-#endif
